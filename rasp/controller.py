2261548f72ff5ac36d08f209e241665a3a9c6143
vitorrafael <vrsilveira07@gmail.com>
2020-02-14 17:41:17 -0300

    Add Solar Tracking software.
    - controller.py: Driver that controls the Stepper Motor.
    - i2c_connector.py: Controls the transference of data through I2C.
    - gpio_connecotr.py: Mock up the RPi.GPIO for purpose of testing.
    - solar_tracker.py: Connects components and build the Solar
                        Tracker application.
    - run.py: Runs the software.
    
    - tests/: Controller and I2C data validation unittests

---
+++ rasp/controller.py
@@ -0,0 +1,170 @@
+import logging
+import logging.config
+
+import time
+
+from enum import Enum
+
+import rasp.gpio_connector as GPIO
+
+logging.config.fileConfig(fname='logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+class Driver:
+    """
+    Class that represents a Stepper Driver, which controls the behavior
+    of the Stepper Motor.
+    """
+
+    def __init__(self, output_pins, step_mode):
+
+        self.movements = {
+            "stop": Movement.stop,
+            "right": Movement.right,
+            "left": Movement.left
+        }
+
+        self.steps = {
+            "halfstep": StepperMode.HALFSTEP,
+        }
+
+        self.output_pins = output_pins
+
+        self.set_step_mode(step_mode)
+
+        GPIO.setmode(GPIO.BCM)
+
+        self.initialize_output_pins()
+
+        GPIO.setwarnings(False)
+
+    def set_step_mode(self, step_mode):
+        """
+
+        Set self.step_mode to the given step_mode if it is a valid type.
+        
+        Arguments:
+            step_mode {string} -- String naming the step type
+        
+        Raises:
+            InvalidStepModeException: Step type not defined in the Steps enum
+        """
+
+        if step_mode not in self.steps:
+            raise InvalidStepModeException
+        else:
+            self.step_mode = step_mode
+
+    def move(self, direction):
+        """
+        Rotates the solar modules axis to a given direction.
+
+        
+        Arguments:
+            direction {string} -- String naming the direction the axis should
+                                  rotate
+        
+        Raises:
+            InvalidMovementException: Movement not defined in the Movement enum
+        """
+
+        if direction not in self.movements.keys():
+            raise InvalidMovementException
+        else:
+            self.movements[direction](self.output_pins,
+                                      self.steps[self.step_mode].value)
+
+    def initialize_output_pins(self):
+        """
+        Initializes the pins as output and set the energy to low.
+        """
+        for pin in self.output_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)
+
+
+class Movement(Enum):
+    """
+    Enumerator with the movements executed by the Stepper motor.
+    """
+
+    @classmethod
+    def stop(cls, output_pins, steps):
+        """
+        Stops the panel axis' movements
+        
+        Arguments:
+            output_pins {list} -- List with the output pins
+        """
+
+        for pin in output_pins:
+            GPIO.output(pin, GPIO.LOW)
+
+        logger.info("Stopped the movements")
+
+    @classmethod
+    def left(cls, output_pins, steps):
+        """ 
+        Rotates the panels clockwise.
+        
+        Arguments:
+            output_pins {list} -- List of output pins
+            steps {list} -- 2D list of the steps
+        """
+
+        for halfstep in range(len(steps)):
+
+            for pin in range(len(output_pins)):
+        
+                GPIO.output(output_pins[pin], steps[halfstep][pin])
+
+            time.sleep(0.001)
+
+        logger.info("Turning the panels to the left")
+
+    @classmethod
+    def right(cls, output_pins, steps):
+        """
+        Rotate the panels anti-clockwise
+
+        Arguments:
+            output_pins {list} -- List of output pins
+            steps {list} -- 2D list of the steps
+        """
+
+        for halfstep in reversed(range(len(steps))):
+
+            for pin in range(len(output_pins)):
+        
+                GPIO.output(output_pins[pin], steps[halfstep][pin])
+
+            time.sleep(0.001)
+
+        logger.info("Turning the panels to the right")
+
+
+class StepperMode(Enum):
+    """
+    Enumerator containg the modes for the steppers motor.
+    """
+    HALFSTEP = [[1, 0, 0, 0],
+                [1, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 1],
+                [0, 0, 0, 1],
+                [1, 0, 0, 1]]
+
+
+class InvalidMovementException(Exception):
+
+    def __str__(self):
+        return "InvalidMovementException: Invalid Movement."
+
+
+class InvalidStepModeException(Exception):
+
+    def __str__(self):
+        return "InvalidStepModeException: Invalid Step Mode."
---
+++ rasp/run.py
@@ -0,0 +1,16 @@
+from rasp.solar_tracker import SolarTracker
+
+if __name__ == "__main__":
+    ARDUINO_ADDRESS = 0X08
+    ACCEPTABLE_DEVIATION = 50
+    OUTPUT_PINS = [21,20,16,12]
+    STEP_MODE = "halfstep"
+    LDR_COUNT = 2
+
+    solar_tracker = SolarTracker(address=ARDUINO_ADDRESS,
+                                 acceptable_deviation=ACCEPTABLE_DEVIATION,
+                                 output_pins=OUTPUT_PINS,
+                                 step_mode=STEP_MODE,
+                                 ldr_count=LDR_COUNT)
+
+    solar_tracker.run(strategy="greedy")
